878af7064da12f4bb87916a0a51c4f406fcc5aa8
Rico <dinosaurgaming35@gmail.com>
2025-12-14 18:06:39 +0200

    first commit

---
+++ MissileStock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchMissile
+{
+    class MissileStock<Tlaunch> where Tlaunch : ILaunchStrategy, new()
+    {
+        private Stack<Missile> _stock;
+        private readonly ILaunchStrategy _launchStrategy;
+        public MissileStock(ILaunchStrategy Tlaunch)
+        {
+            _launchStrategy = Tlaunch;
+            _stock = new Stack<Tlaunch>;
+        }
+        public void AddMissile() => _stock.Push(new Missile(_launchStrategy));
+        public void AddMissiles(int amount) {
+            for (int i = 0; i < amount; i++) {
+                AddMissile();
+            }
+        }
+        public Missile UseMissile() {
+            if (_stock.Count > 0)
+            {
+                return _stock.Pop();
+            }
+            throw new InvalidOperationException();
+        }
+        public List<Missile> UseMissiles(int amount)
+        {
+            if (_stock.Count < amount) throw new InvalidOperationException();
+            List<Missile> missiles = new List<Missile>();
+            for (int i = 0; i < amount; i++)
+            {
+                missiles.Add(_stock.Pop());
+            }
+            return missiles;
+        }
+    }
+}
---
+++ S6ML.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchMissile
+{
+    internal class S6ML
+    {
+    }
+}
