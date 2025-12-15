76935c5d746c46c2e245cb8c11ce2f2cbc9b0352
Rico <dinosaurgaming35@gmail.com>
2025-12-15 15:23:49 +0200

    basic menu

--- MenuActions/LaunchMissiles.cs
+++ MenuActions/LaunchMissiles.cs
@@ -17,4 +17,3 @@ namespace LaunchMissile.MenuActions
         }
     }
 }
-s
--- S6ML.cs
+++ S6ML.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MenuBuilder.BLL.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace LaunchMissile
 {
     class S6ML
     {
+        private static Menu _mainMenu = Menu.MainMenu("Shayetet 6 Missile Launcher");
+        public S6ML()
+        {
+            _mainMenu.SubMenu("Add Missiles");
+            _mainMenu.SubMenu("Launch Missiles");
+            _mainMenu.SubMenu("Add Missiles");
+        }
     }
 }
